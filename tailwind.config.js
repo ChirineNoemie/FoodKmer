bd1114043c899d27fcd124852d3b66b2f342a456
ChirineNoemie <noemiewafo8@gmail.com>
2023-09-15 00:24:46 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#06b6d4',
+        secondary:{
+          100: '#9333ea',
+          200: '#2563eb'
+        },
+      },
+      fontSize:{
+        pro: '10rem'
+      },
+      fontFamily:{
+        nunitro:'Nunito'
+      }
+    },
+  },
+  plugins: [],
+}
+
